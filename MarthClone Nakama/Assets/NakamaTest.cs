a2c5d99a78ef4b24b7325bf4e14406667782c974
Jackson Katzman <mourningwood349@gmail.com>
2019-04-15 16:51:39 -0500

    First Networking Tests

--- MarthClone Nakama/Assets/NakamaTest.cs
+++ MarthClone Nakama/Assets/NakamaTest.cs
@@ -9,6 +9,7 @@ public class NakamaTest : MonoBehaviour
 
     private IClient _client = new Client("defaultkey", "18.224.212.203", 7350, false);
     private ISession _session;
+    private ISocket _socket;
 
     private async void Awake()
     {
@@ -21,12 +22,41 @@ public class NakamaTest : MonoBehaviour
             {
                 _session = session;
                 Debug.Log(_session);
+                Debug.Log("Hit this weird place.");
                 return;
             }
         }
         var deviceid = SystemInfo.deviceUniqueIdentifier;
         _session = await _client.AuthenticateDeviceAsync(deviceid);
         PlayerPrefs.SetString(PrefKeyName, _session.AuthToken);
+        _socket = _client.CreateWebSocket();
+        _socket.OnConnect += (sender, args) =>
+        {
+            Debug.Log("Socket connected.");
+        };
+        _socket.OnDisconnect += (sender, args) =>
+        {
+            Debug.Log("Socket disconnected.");
+        };
+        await _socket.ConnectAsync(_session);
         Debug.Log(_session);
+    }
+
+    public async void AttemptMatchmake()
+    {
+        Debug.Log("Begin Attempted Matchmaking!");
+        var query = "*";
+        var minCount = 2;
+        var maxCount = 2;
+  
+
+        var matchmakerTicket = await _socket.AddMatchmakerAsync(
+            query, minCount, maxCount);
+        _socket.OnMatchmakerMatched += (_, matched) =>
+        {
+            Debug.LogFormat("Received MatchmakerMatched message: {0}", matched);
+            var opponents = string.Join(",", matched.Users); // printable list.
+            Debug.LogFormat("Matched opponents: {0}", opponents);
+        };
     }
 }
